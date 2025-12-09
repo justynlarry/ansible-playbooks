52ba7b6e02a73f943da42df133edc1bb832ce8a7
justynlarry <justynlarry@gmail.com>
2025-12-09 15:55:04 -0700

    Updated inventory_extract.sh path to .txt file.

--- bash_scripts/inventory_extract.sh
+++ bash_scripts/inventory_extract.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep 'ansible_host=' /home/blue-user/ansible-playbooks/inventories/production.ini | awk -F'ansible_host=' '{print $2}' | awk '{print $1}' | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$' > /tmp/prod_hosts_to_exclude.txt
+grep 'ansible_host=' /home/blue-user/ansible-playbooks/inventories/production.ini | awk -F'ansible_host=' '{print $2}' | awk '{print $1}' | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$' > /home/blue-user/ansible-playbooks/inventories/prod_hosts_to_exclude.txt
